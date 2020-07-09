28460efd0f8d4e75f7fde0f5191cd7a2fd3893a6
fahadkhan917 <fk16181@gmail.com>
2020-07-10 08:22:58 +1000

    update

---
+++ class 17/app.js
@@ -0,0 +1,34 @@
+//setInterval => reapeatively running
+//setTimeout => after sometime
+
+// var count=0;
+// var interval; 
+// function time(){
+//     count++;
+//     document.write(count);
+// document.write("hello");
+
+
+// }
+
+// interval=setInterval(time,1000); 
+
+// setTimeout(function(){
+//     clearInterval(interval)
+// },10000)
+
+// // function timeout(){
+
+// // document.write("1 bar chala ga");
+
+
+// // }
+
+
+// setTimeout(timeout,1000);
+
+
+
+
+
+
