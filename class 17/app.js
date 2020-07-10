3163bd5335f5cba888a406e754b28d962d5f9ca7
fahadkhan917 <fk16181@gmail.com>
2020-07-11 00:11:09 +1000

    update

--- class 17/app.js
+++ class 17/app.js
@@ -33,15 +33,47 @@ var msec=0;
 var minhead=document.getElementById("min");
 var sechead=document.getElementById("sec");
 var msechead=document.getElementById("msec");
-var inter
+var interval;
 
 function timer(){
 msec++;
-msechead.in
+msechead.innerHTML=msec;
+if(msec>=100){
+sec++;
+sechead.innerHTML=sec;
+msec=0;
+}
+else if(sec>=60){
+min++;
+sec=0;
+minhead.innerHTML=min;
+
+
+}
+
+}
+
+function start(){
+
+    interval=setInterval(timer,10)
+}
+
+function stop(){
+clearInterval(interval)
 
 }
 
 
+function reset(){
+min=0;
+sec=0;
+msec=0;
+minhead.innerHTML=min;
+sechead.innerHTML=sec;
+msechead.innerHTML=msec;
+stop();
+
+}
 
 
 
