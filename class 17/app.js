2b16f152853dce3b12c0264139674cbfba9789cf
fahadkhan917 <fk16181@gmail.com>
2020-07-12 12:06:09 +1000

    update

--- class 17/app.js
+++ class 17/app.js
@@ -27,53 +27,61 @@
 
 // setTimeout(timeout,1000);
 
-var min=0;
-var sec=0;
-var msec=0;
-var minhead=document.getElementById("min");
-var sechead=document.getElementById("sec");
-var msechead=document.getElementById("msec");
-var interval;
+// var min=0;
+// var sec=0;
+// var msec=0;
+// var minhead=document.getElementById("min");
+// var sechead=document.getElementById("sec");
+// var msechead=document.getElementById("msec");
+// var interval;
 
-function timer(){
-msec++;
-msechead.innerHTML=msec;
-if(msec>=100){
-sec++;
-sechead.innerHTML=sec;
-msec=0;
-}
-else if(sec>=60){
-min++;
-sec=0;
-minhead.innerHTML=min;
-
-
-}
-
-}
-
-function start(){
-
-    interval=setInterval(timer,10)
-}
-
-function stop(){
-clearInterval(interval)
-
-}
+// function timer(){
+// msec++;
+// msechead.innerHTML=msec;
+// if(msec>=100){
+// sec++;
+// sechead.innerHTML=sec;
+// msec=0;
+// }
+// else if(sec>=60){
+// min++;
+// sec=0;
+// minhead.innerHTML=min;
 
 
-function reset(){
-min=0;
-sec=0;
-msec=0;
-minhead.innerHTML=min;
-sechead.innerHTML=sec;
-msechead.innerHTML=msec;
-stop();
+// }
 
-}
+// }
+
+// function start(){
+
+//     interval=setInterval(timer,10);
+//     var st=document.getElementById("strt").style.visibility="hidden";
+// }
+
+// function stop(){
+// clearInterval(interval)
+
+// }
+
+
+// function reset(){
+// min=0;
+// sec=0;
+// msec=0;
+// minhead.innerHTML=min;
+// sechead.innerHTML=sec;
+// msechead.innerHTML=msec;
+// stop();
+
+// }
+
+///dom
+
+//Documnnt object model
+
+
+// console.log(document.childNodes[1].childNodes[1].childNodes[0] );
 
 
 
