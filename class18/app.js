84a510009ea4162020128c7de5a342ef90fa62ec
fahadkhan917 <fk16181@gmail.com>
2020-07-15 09:08:38 +1000

    update

--- class18/app.js
+++ class18/app.js
@@ -1,7 +1,12 @@
-function a(){
+// function a(){
 
 
-var ele=document.getElementById("ab");
-ele.disabled=true;
+// var ele=document.getElementById("ab");
+// ele.disabled=true;
 
-}
+// }
+
+
+
+// var ele=document.getElementsByTagName("p");
+// ele[0].innerHTML="fahad";
