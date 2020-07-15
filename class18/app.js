1a408e41fbc39dfb784a5dc195ff928b77f9a543
fahadkhan917 <fk16181@gmail.com>
2020-07-15 19:44:35 +1000

    updated

--- class18/app.js
+++ class18/app.js
@@ -10,3 +10,11 @@
 
 // var ele=document.getElementsByTagName("p");
 // ele[0].innerHTML="fahad";
+
+var ele=document.body.childNodes[13];
+var b=ele.nodeType;
+if(b==1){
+
+ele.innerHTML="fahad";
+
+}
