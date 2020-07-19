db51fb9875e1270a3cabdd7d00bc00158100c0b6
fahadkhan917 <fk16181@gmail.com>
2020-07-20 07:41:15 +1000

    update

---
+++ helpdesk/helpdesk5.js
@@ -0,0 +1,13 @@
+// function abc(a){
+
+// var bt=document.getElementById("val");
+// bt.value+=a;
+
+// }
+
+// function xyz(){
+
+// var a=document.getElementById("val");
+// a.value=eval(a.value);
+
+// }
