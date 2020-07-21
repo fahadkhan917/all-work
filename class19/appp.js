aeaf39acde38981ab239521e4e99bf3d5f70e435
fahadkhan917 <fk16181@gmail.com>
2020-07-21 22:49:49 +1000

    update

---
+++ class19/appp.js
@@ -0,0 +1,16 @@
+// todo application cover
+
+// object
+
+
+
+var web={
+// properties :value    
+t1:"ali",
+t2:"fahad",
+t3:"asad"
+
+}
+web.t4="umer";//Add one
+// array aslo return object
+document.write(web.t4);
