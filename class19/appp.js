5b7e5bc1beaf7a4fc257dbcabfc35c2e1e958c73
fahadkhan917 <fk16181@gmail.com>
2020-07-21 22:54:41 +1000

    update

--- class19/appp.js
+++ class19/appp.js
@@ -11,6 +11,7 @@ t2:"fahad",
 t3:"asad"
 
 }
-web.t4="umer";//Add one
+web.t4="umer";//Add one string
+web.t5=["ahad","umair"];//add array in object
 // array aslo return object
-document.write(web.t4);
+document.write(web.t5[0]);
