82c54c08bfbec6fc1ea9c1348391405812b59977
fahadkhan917 <fk16181@gmail.com>
2020-07-22 13:40:35 +1000

    Update appp.js

--- class19/appp.js
+++ class19/appp.js
@@ -4,14 +4,33 @@
 
 
 
-var web={
-// properties :value    
-t1:"ali",
-t2:"fahad",
-t3:"asad"
+// var web={
+// // properties :value    
+// t1:"ali",
+// t2:"fahad",
+// t3:"asad"
 
-}
-web.t4="umer";//Add one string
-web.t5=["ahad","umair"];//add array in object
-// array aslo return object
-document.write(web.t5[0]);
+// }
+// web.t4="umer";//Add one string
+// web.t5=["ahad","umair"];//add array in object
+// // array aslo return object
+// document.write(web.t5[0]);
+
+// //check a porpertie in object
+// var w="t5" in web;
+// document.write(w);
+
+
+var myQuestion=[
+    {
+  question:"what is your name",
+  answer:{
+ a:"fahad",
+ b:"asad",
+ c:"umer"
+
+  },
+cor:"c"
+
+    }
+]
