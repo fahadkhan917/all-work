b193c6167a0524ef6668bdd715704efce42ff859
fahadkhan917 <fk16181@gmail.com>
2020-07-29 08:35:08 +1000

    Update app.js

--- pra/app.js
+++ pra/app.js
@@ -115,3 +115,19 @@
 // document.write(i);
 
 // }
+
+// for(var i=0;i<=20;i=i+2){
+// if(i%2==0){
+// document.write(i)
+
+// }
+
+// }
+
+for(var i=0;i<20;i++){
+if(i%2==1){
+document.write(i);
+
+}
+
+}
