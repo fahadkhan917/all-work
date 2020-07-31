1d6cd51a7f597521f5838d7893a72bf821e16bb7
fahadkhan917 <fk16181@gmail.com>
2020-08-01 06:01:43 +1000

    Update app.js

--- pra/app.js
+++ pra/app.js
@@ -274,3 +274,13 @@
 // var b=a.charAt(3);
 // document.write(b);
 
+
+// var a="hyderabad";
+// var b=a.replace('hyder','islama');
+// document.write(b);
+
+// var message = "Ali and Sami are best friends. They play cricket and football together";
+// var b=message.replace(/and/g,'&');
+// document.write(b);
+
+
