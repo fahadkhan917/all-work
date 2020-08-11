777e6cb855f89f0b9f786cee0c7dfb2d3293a4d2
fahadkhan917 <fk16181@gmail.com>
2020-08-11 14:04:00 +0500

    Update app.js

--- pra/app.js
+++ pra/app.js
@@ -1258,3 +1258,8 @@
 // var message = "Ali and Sami are best friends. They play cricket and football together.";
 // var b=message.replace(/and/g,'&');
 // document.write(b);
+
+// var a="peanuts";
+// var b=a.toUpperCase();
+// document.write(b);
+
