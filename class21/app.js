7f645377868192ba8a2e8a02f8371fca6f4a7a0d
fahadkhan917 <fk16181@gmail.com>
2020-08-14 20:03:06 +0500

    Update app.js

--- class21/app.js
+++ class21/app.js
@@ -25,3 +25,33 @@
 // var student2=new Student("ahad",123);
 // console.log(student1);
 // console.log(student2);
+
+// function Student(name,roll){
+//     this.name=name;
+//     this.roll=roll;
+//     this.getName=function(){
+
+// return this.name;
+
+//     }
+    
+//     }
+
+//     var student1=new Student("fahad",456);
+//     var student2=new Student("ahad",123);
+//     console.log(student2.getName())
+// function Student(name,roll){
+//         this.name=name;
+//         this.roll=roll;
+    
+
+//     Student.prototype.getName=function(){
+
+//     return this.name;
+
+    
+// }
+// }
+
+// var student1=new Student("fahad",456);
+// console.log(student1.getName())
