5ec6bb80816f61ed525c77867e8651bdb152c377
fahadkhan917 <fk16181@gmail.com>
2020-08-14 14:27:31 +0500

    update

---
+++ class21/app.js
@@ -0,0 +1,27 @@
+
+
+// var a={
+
+// name:"fahad",
+// age:21,
+// abc:function(){
+
+//     document.write("umer")
+
+// }
+
+// }
+// document.write(a.abc())
+
+
+// function Student(name,roll){
+// this.name=name;
+// this.roll=roll;
+
+
+// }
+
+// var student1=new Student("fahad",456);
+// var student2=new Student("ahad",123);
+// console.log(student1);
+// console.log(student2);
