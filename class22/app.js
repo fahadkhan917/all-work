da32a7472eb93ddb62650de14da6977ddec5a467
fahadkhan917 <fk16181@gmail.com>
2020-08-25 23:50:29 +0500

    Update app.js

--- class22/app.js
+++ class22/app.js
@@ -1,7 +1,11 @@
 function press(){
 
-var locate=window.location.href
-// console.log(locate);
-window.location.href='https://www.google.com/';
+// var locate=window.location.href
+// // console.log(locate);
+// window.location.href='https://www.google.com/';
+
+
+// window.location.assign('https://www.google.com/');
+window.location.replace('https://www.google.com/');
 
 }
