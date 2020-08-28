53fc49e029074705535f45ae30284e7bb2310b72
fahadkhan917 <fk16181@gmail.com>
2020-08-28 20:43:55 +0500

    update

--- helpdesk/helpdesk8.js
+++ helpdesk/helpdesk8.js
@@ -1,17 +1,41 @@
-var obj={
-name:"fahad",
-id:123,
-a:[45,78,98,78],
-abc:function(){
+// var obj={
+// name:"fahad",
+// id:123,
+// a:[45,78,98,78],
+// abc:function(){
 
-document.write("hello");
+// document.write("hello");
 
-}
+// }
 
 
-}
-for(var key in obj){
+// }
+// for(var key in obj){
 
-document.write(obj[key])
+// document.write(key,obj[key]) 
 
-}
+// }
+
+// var a=[45,78,89,78];
+// var sum=0;
+// for(var i=0;i<a.length;i++){
+// sum=a[i];
+//   sum+=sum+a[i]  
+
+
+// }
+// document.write(sum);
+
+// var a=prompt("enter the user");
+// for(var i=0;i<a.length;i++){
+
+//     if(a.charCodeAt(i)===97){
+
+//   document.write("yes");
+
+//     }
+
+// }
+
+
+
