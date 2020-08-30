0c7a1d69233c136b93ded59ca7cc0bb98f14c2ec
fahadkhan917 <fk16181@gmail.com>
2020-08-30 11:32:47 +0500

    update

---
+++ class23/app.js
@@ -0,0 +1,19 @@
+//Ecmascript6=2015
+//es6 
+// var name="asad";
+// function foo(){
+
+// name="fahad";
+
+// }
+// foo();
+// console.log(name);
+
+
+if(true){
+
+let fullname="fahad";
+
+}
+
+console.log(fullname);
