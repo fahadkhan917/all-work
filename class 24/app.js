3a18547d8d4fcbfd28ba8504d558a1b4707cd939
fahadkhan917 <fk16181@gmail.com>
2020-09-09 14:46:16 +0500

    Update app.js

--- class 24/app.js
+++ class 24/app.js
@@ -1,8 +1,12 @@
-let obj={
-name:"fahad",
-cls:123
+// let obj={
+// name:"fahad",
+// cls:123
 
-}
+// }
 
-let{name,cls}=obj;
-document.write(name);
+// let{name,cls}=obj;
+// document.write(name);
+
+// let name=["fahad","ali","khan"];
+// let[name1,name2,name3]=name;
+// document.write(name3)
