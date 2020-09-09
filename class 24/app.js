bd4bc983235ca74f17ac8c89e40de40bb16da8f4
fahadkhan917 <fk16181@gmail.com>
2020-09-10 00:20:52 +0500

    Update app.js

--- class 24/app.js
+++ class 24/app.js
@@ -9,4 +9,52 @@
 
 // let name=["fahad","ali","khan"];
 // let[name1,name2,name3]=name;
-// document.write(name3)
+// document.write(name3);
+
+
+//function expression
+
+//  let foo=function(num,num1){
+
+// document.write("hellow");
+
+//  }
+
+//  foo(); 
+
+// arrow function
+
+// let hello=(name,name1)=>{
+
+// document.write(`${name} ${name1}`);
+// }
+ 
+// hello("fahad","khan");
+
+// let a=()=>{
+
+// return "fahad";
+
+// }
+
+// a();
+
+// let a=()=> "fahad";
+    
+    
+    
+//     a();
+
+ 
+// let a=(name)=>{
+
+//     console.log(this)
+
+// }
+// a();
+
+
+
+// setInterval(() => {
+//     document.write("fahad");
+// }, 1000);
