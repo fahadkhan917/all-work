c941b3c913161ab92a8f0cdb95edffeb91a932f2
fahadkhan917 <fk16181@gmail.com>
2020-09-22 14:58:17 +0500

    update

--- first/src/App.js
+++ first/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-   <h1>hellow world</h1>
-    </div>
-  );
+// class App extends React.Component{
+// render(){
+//   return(
+// <div>
+
+// <h1>hello</h1>
+
+// </div>
+//   )
+
+// }
+
+
+// }
+
+
+function App(){
+  let finame="ali";
+  let lname="khan";
+  let arr=[1,4,5,7,8,65];
+return(
+<div>
+<h2>{`${finame}${lname}`}</h2>
+<h2>{arr}</h2>
+</div>
+
+
+)
+
+
+
 }
 
 export default App;
