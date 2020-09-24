fcc8a4d5077de6d5a0f5a286648a236eb7563592
fahadkhan917 <fk16181@gmail.com>
2020-09-24 17:16:39 +0500

    Update App.js

--- first/src/App.js
+++ first/src/App.js
@@ -17,29 +17,54 @@ import './App.css';
 // }
 
 
-function App(){
-  let finame="ali";
-  let lname="khan";
-  let arr=[1,4,5,7,8,65];
-  var sum=0;
-  for(var i=0;i<arr.length;i++){
+// function App(){
+  // let finame="ali";
+  // let lname="khan";
+  // let arr=[1,4,5,7,8,65];
+  // var sum=0;
+  // for(var i=0;i<arr.length;i++){
 
-   sum+=sum+arr[i];
+  //  sum+=sum+arr[i];
 
-  }
+  // }
 
 
+ 
 
-return(
-<div>
-<h2>{`${finame}${lname}`}</h2>
+
+// return(
+{/* <div> */}
+{/* <h2>{`${finame}${lname}`}</h2>
 <h2>{arr}</h2>
 
-<h2>{sum}</h2>
+<h2>{sum}</h2> */}
+
+
+
+// </div>
+
+
+// )
+
+
+
+// }
+
+
+class App extends React.Component{
+render(){
+return(
+
+<div>
+<h1>hellow</h1>
+
 </div>
 
-
 )
+
+
+
+}
 
 
 
