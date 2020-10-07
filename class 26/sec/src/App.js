8dbd0cf04ceee63918766f22c5bddb4cd3ddd823
fahadkhan917 <fk16181@gmail.com>
2020-10-07 20:06:59 +0500

    Update App.js

--- class 26/sec/src/App.js
+++ class 26/sec/src/App.js
@@ -7,10 +7,17 @@ constructor(){
 super()
 this.state={
 name:"fahad",
-email:"fk16181@gmail.com"
+email:"fk16181@gmail.com",
+a:["ahad","ali","umer","asad"]
 
 
 }
+}
+
+set_name(){
+
+console.log("run")
+
 }
 
 render(){
@@ -20,7 +27,10 @@ return(
   <Header/>
 <h1>hellow</h1>
 <h3>{this.state.name}</h3>
+<h4>my email is{this.state.email}</h4>
+<h5>my array is{this.state.a}</h5>
 <input type="text" placeholder="enter the value"/>
+<button onClick={this.set_name}>Setname</button>
 <Doe/>
 </div>
 
