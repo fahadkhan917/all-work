f04dd6b939fd0942eb08f773d44b086fc3079a54
fahadkhan917 <fk16181@gmail.com>
2020-10-08 01:33:52 +0500

    Update App.js

--- class 26/sec/src/App.js
+++ class 26/sec/src/App.js
@@ -8,15 +8,15 @@ super()
 this.state={
 name:"fahad",
 email:"fk16181@gmail.com",
-a:["ahad","ali","umer","asad"]
-
+a:["ahad","ali","umer","asad"],
+value:""
 
 }
 }
 
 set_name=()=>{
 this.setState({
-name:"ali"
+name:this.state.value
 
 })
 
@@ -29,6 +29,17 @@ get_name=()=>{
 
 }
 
+handlechange=(e)=>{
+this.setState({
+[e.target.name]: e.target.value
+
+
+})
+
+
+}
+
+
 render(){
 return(
 
@@ -38,7 +49,9 @@ return(
 <h3>{this.state.name}</h3>
 <h4>my email is{this.state.email}</h4>
 <h5>my array is{this.state.a}</h5>
-<input onChange={(e)=>this.setState({name:e.target.value })} type="text" placeholder="enter the value"/>
+{/* <input onChange={(e)=>this.setState({name:e.target.value })} type="text" placeholder="enter the value"/> */}
+<input name="name" onChange={(e)=>this.handlechange(e)} type="text" placeholder="enter the name"/>
+<input name="email" onChange={(e)=>this.handlechange(e)} type="text" placeholder="enter the email"/>
 <button onClick={this.set_name}>Setname</button>
 <button onLoad={this.get_name}>get name</button>
 <Doe/>
