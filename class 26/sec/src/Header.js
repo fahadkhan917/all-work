2e860bd4bd1e132fe04a81bdd18cb8471f1272a5
fahadkhan917 <fk16181@gmail.com>
2020-10-07 16:37:24 +0500

    update

--- class 26/sec/src/Header.js
+++ class 26/sec/src/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Logo from './image/download.png';
+
+
+
 
 
 class Header extends React.Component{
 render(){
+    
 return(
 
 <div>
-
+<img src={Logo}/>
 <h1>pagal</h1>
 
 </div>
@@ -29,15 +35,20 @@ render(){
     var a=5;
     var b=5;
     var c=a+b;
-    var z=prompt("enter the user");
+ 
 return(
 
    <div>
    <h1>{c}</h1>
-<h2>{z}</h2>
+   <img src={Logo}/>
    <h1>Does</h1>
+   <Button variant="outlined">Default</Button>
+<Button variant="outlined" color="primary">
+  Primary
+</Button>
 
    </div> 
+   
 
 
 )
@@ -47,6 +58,8 @@ return(
 
 
 }
+
+
 
 
 export{
