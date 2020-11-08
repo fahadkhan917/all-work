e9def3e669bf36ef90463016f65c1c6395dc5dee
fahadkhan917 <fk16181@gmail.com>
2020-11-08 16:55:12 +0500

    Update app.js

---
+++ es6_pra/app.js
@@ -0,0 +1,41 @@
+// varaible Es6
+// var name="asad";
+// function foo(){
+
+//  name="fahad"
+
+// }
+
+// foo()
+
+// console.log(name)
+
+
+
+//let varaible
+// if(true){
+
+// let names="fahad"
+
+// }
+
+// console.log(names)
+
+// let finame="fahad";
+// finame="khan";
+// console.log(finame)
+
+
+
+// const school="abc school";
+// var obj={
+
+// schoolname:school
+
+// }
+
+// for(var key in obj){
+
+// document.write(obj[key])
+
+// }
