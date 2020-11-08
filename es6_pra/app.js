5b3c07d62f634eea282ac782e70aebc21d6d7346
fahadkhan917 <fk16181@gmail.com>
2020-11-08 19:36:03 +0500

    Update app.js

--- es6_pra/app.js
+++ es6_pra/app.js
@@ -75,3 +75,68 @@
 // document.write(obj1[key])
 
 // }
+
+
+//  object desturing
+
+
+// var obj={
+
+// name:"fahad",
+// id:123,
+// school:"abcd school"
+
+
+// }
+
+// let{name,id,school}=obj;
+// document.write(name);
+// console.log(id); 
+
+
+// var age=20;
+// if(age>30){
+
+// console.log("30sa bara hai")
+
+// }
+
+// else{
+
+// console.log("30 sy chota hai")
+
+// }
+
+// var age=20;
+// var check=
+// age>30?
+// "30 sa bara hai"
+// :
+// "30  sa chota hai"
+// console.log(check)
+
+// console.log("fahad")
+// console.log("fahadkhan")
+// console.log("fahadumer")
+
+
+// async or sync
+ 
+// setTimeout(() => {
+//    console.log("fahad") 
+// }, 1000);
+
+// console.log("fahadkhan")
+// console.log("asad")
+
+// var name1;
+// setTimeout(() => {
+//     name1="fahad"
+// }, 2000);
+// setInterval(() => {
+//     console.log(name1)    
+// }, 1000);
+
+
+//promise
+ 
